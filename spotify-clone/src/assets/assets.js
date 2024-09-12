f340dd4385b38afd5ab0fd71c1fe4c28b60c3334
Arkadyuti Chaudhuri <Arkadyutichaudhuri@gmail.com>
2024-09-13 02:29:58 +0530

    done

---
+++ spotify-clone/src/assets/assets.js
@@ -0,0 +1,186 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.jpeg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpeg'
+import img4 from './img4.jpeg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpeg'
+import img7 from './img7.jpeg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import img10 from './img10.jpg'
+import img11 from './img11.jpg'
+import img12 from './img12.jpeg'
+import img13 from './img13.jpg'
+import img14 from './img14.jpeg'
+import img15 from './img15.jpg'
+import img16 from './img16.jpg'
+import song1 from  './song1.mp3'
+import song2 from  './song2.mp3'
+import song3 from  './song3.mp3'
+import song4 from  './song4.mp3'
+import song5 from  './song5.mp3'
+import song6 from  './song6.mp3'
+import song7 from  './song7.mp3'
+import song8 from  './song8.mp3'
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Top 50 Global",
+        image: img8,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "Top 50 India",
+        image: img9,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#22543d"
+    },
+    {   
+        id:2,
+        name: "Trending India",
+        image: img10,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#742a2a"
+    },
+    {   
+        id:3,
+        name: "Trending Global",
+        image: img16,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#44337a"
+    },
+    {   
+        id:4,
+        name: "Mega Hits,",
+        image: img11,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#234e52"
+    },
+    {   
+        id:5,
+        name: "Happy Favorites",
+        image: img15,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#744210"
+    }
+]
+export const songsData = [
+    {
+        id: 0,
+        name: "Blinding Lights",
+        image: img1,
+        file: song1,
+        desc: "The Weeknd's chart-topping synthwave anthem with '80s nostalgia.",
+        duration: "3:22"
+    },
+    {
+        id: 1,
+        name: "Night Changes",
+        image: img2,
+        file: song2,
+        desc: "One Direction's heartfelt ballad about growing up and life's changes.",
+        duration: "3:46"
+    },
+    {
+        id: 2,
+        name: "Wake Me Up",
+        image: img3,
+        file: song3,
+        desc: "Avicii's iconic blend of EDM and country-folk with uplifting vibes.",
+        duration: "4:07"
+    },
+    {
+        id: 3,
+        name: "Comfortably Numb",
+        image: img4,
+        file: song4,
+        desc: "Pink Floyd's legendary psychedelic rock track with powerful guitar solos.",
+        duration: "6:22"
+    },
+    {
+        id: 4,
+        name: "The Nights",
+        image: img6,
+        file: song5,
+        desc: "Avicii's inspirational anthem encouraging people to live life to the fullest.",
+        duration: "2:56"
+    },
+    {
+        id: 5,
+        name: "Another Brick in the Wall",
+        image: img14,
+        file: song6,
+        desc: "Pink Floyd's protest anthem against rigid schooling and conformity.",
+        duration: "3:59"
+    },
+    {
+        id: 6,
+        name: "Starboy",
+        image: img7,
+        file: song7,
+        desc: "The Weeknd's bold track exploring fame and self-transformation.",
+        duration: "3:50"
+    },
+    {
+        id: 7,
+        name: "The Scientist",
+        image: img12,
+        file: song8,
+        desc: "Coldplay's emotional ballad about heartbreak and reflection.",
+        duration: "5:09"
+    }
+];
